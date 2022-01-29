069dab8d36cdef93d033e1e30e5eaef13e81f77e
egananda <87672592+egananda@users.noreply.github.com>
2022-01-29 11:35:11 +0700

    hapus folder yang tidak diperlukan

--- belajar-php-oop/Object.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once "data/Person.php";
-
-$person = new Person();
-
-var_dump($person);
--- belajar-php-oop/function.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once "data/Person.php";
-
-$person = new Person();
-
-$person->sayHello("Ega");
